3d537ebba65e456fba768d1be3a6eb0866558654
PedroJSilva2001 <pedro.jsilva322@gmail.com>
2024-10-28 21:35:59 +0000

    Remove unused imports and comment print

--- cmender/src/main/java/pt/up/fe/specs/cmender/Launcher.java
+++ cmender/src/main/java/pt/up/fe/specs/cmender/Launcher.java
@@ -1,25 +1,13 @@
 package pt.up.fe.specs.cmender;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import pt.up.fe.specs.cmender.cli.CliArgsParser;
 import pt.up.fe.specs.cmender.cli.CliArgsParserException;
 import pt.up.fe.specs.cmender.cli.CliReporting;
-import pt.up.fe.specs.cmender.data.MendingDirData;
 import pt.up.fe.specs.cmender.logging.Logging;
-import pt.up.fe.specs.cmender.mending.CMenderResult;
 import pt.up.fe.specs.cmender.mending.MendingEngine;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 // TODO replace Path.get() for concatenation of subpaths with resolve() as the latter seems robust and is able to normalize the path as well
 public class Launcher {
-    private static void saveResults(CMenderInvocation invocation, MendingDirData mendingDirData, CMenderResult result) {
-    }
-
     public static void main(String[] args) {
         var properties = CMenderProperties.get();
 
@@ -59,8 +47,7 @@ public class Launcher {
         for (int i = 0; i < n; i++) {
             var result = new MendingEngine(invocation).execute();
 
-
-            System.out.println(result);
+            //System.out.println(result);
             //diagExporterTotal += result.diagExporterTotalTimeMs();
             //total += result.totalTimeMs();
         }
