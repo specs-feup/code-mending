73cd6556c8de8d2d45742932360afbf1d4c2bc89
PedroJSilva2001 <pedro.jsilva322@gmail.com>
2025-01-09 14:45:36 +0000

    Add eval to all analysis impls

--- evaluation/eval.sh
+++ evaluation/eval.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-make evaluate EVAL_OUTPUT_PATH=../results/evaluation TUPATCHER_OUTPUT_PATH=../../clava/TranslationUnitPatcher/output CMENDER_OUTPUT_PATH=../results/cmender_output_multiple_pp_errors
+make evaluate EVAL_OUTPUT_PATH=../results/evaluation/first TUPATCHER_OUTPUT_PATH=../../clava/TranslationUnitPatcher/output CMENDER_OUTPUT_PATH=../results/cmender_output_first_error
+make evaluate EVAL_OUTPUT_PATH=../results/evaluation/multiplepp TUPATCHER_OUTPUT_PATH=../../clava/TranslationUnitPatcher/output CMENDER_OUTPUT_PATH=../results/cmender_output_multiple_pp_errors
+make evaluate EVAL_OUTPUT_PATH=../results/evaluation/multiple TUPATCHER_OUTPUT_PATH=../../clava/TranslationUnitPatcher/output CMENDER_OUTPUT_PATH=../results/cmender_output_multiple_errors
